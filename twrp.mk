5c0c9eae5f1169bb33fc6021d730267e14bcad61
Andreas Schneider <asn@cryptomilk.org>
2020-02-27 09:02:31 +0100

    Set PLATFORM_VERSION for decryption of lineage-17.1

--- twrp.mk
+++ twrp.mk
@@ -98,5 +98,6 @@ TW_THEME := portrait_hdpi
 # Use a future date for the security patchlevel.  As TWRP doesn't do key
 # upgrades it should just be fine using it for decryption.
 PLATFORM_SECURITY_PATCH := 2025-12-31
+PLATFORM_VERSION := 16.1.0
 
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
